6e66c3d20d2bf3a3fb19c016564ffbb0a6fba7ee
Indra Setyo Wibowo <indra.setyo.ft19@mail.umy.ac.id>
2022-01-10 15:27:01 +0700

    Add project files.

---
+++ ServerChatApp_077_Indra Setyo Wibowo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using WCFService_2Way_077;
+
+namespace ServerChatApp_077_Indra_Setyo_Wibowo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost hostObj = null;
+            try
+            {
+                hostObj = new ServiceHost(typeof(ServiceCallBack));
+                hostObj.Open();
+                Console.WriteLine("Server Siap!!!");
+                Console.ReadLine();
+                hostObj.Close();
+            }
+            catch (Exception ex)
+            {
+                hostObj = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
